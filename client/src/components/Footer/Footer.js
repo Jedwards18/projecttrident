9501dbb791802fc70fe6593ab9cdd079f5314b59
Philip Nguyen <philphanguyen@gmail.com>
2018-10-16 13:35:46 -0400

    fixed up home
    modals not installed yet but front page looks a bit better

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => (
 <div className="ui grid">
     <div className="row">
         <div className="three wide column"></div>
-        <div className="four wide column">
+        <div className="five wide column">
             <p id="footer">
-            Built by Jacob Edwards, Phillip Nguyen and Jeff Anderson.
+            Built by <strong>Jacob Edwards, Phillip Nguyen</strong> and <strong>Jeff Anderson</strong>.;
             </p>
         </div>
     </div>
--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -150,6 +150,21 @@ class Home extends Component {
                         </div>
                     </div>
                 </div>
+                <div class="ui confirm modal">
+                    <div class="content">
+                    <p>New account registered. Welcome!</p>
+                    </div>
+                </div>
+                <div class="ui password modal">
+                    <div class="content">
+                    <p>Please choose a more secure password.</p>
+                    </div>
+                </div>
+                <div class="ui incomplete modal">
+                    <div class="content">
+                    <p>Please complete the form in its entirety.</p>
+                    </div>
+                </div>
             </div>
         )
     }
